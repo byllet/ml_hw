1322d92483487a44eedf769b8344b76797c3231c
VariPor <maiorova.ev@phystech.edu>
2025-05-03 19:13:41 +0300

    add encoder and model

---
+++ hw4/src/encoder.py
@@ -0,0 +1,52 @@
+import random
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+
+def set_seed(seed=42):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    
+set_seed()
+
+
+class EncoderModule(nn.Module):
+    def __init__(self, device, in_channels, out_channels=64, conv_kernel_size=3):
+        super().__init__()
+        self.conv_layer = nn.Conv2d(in_channels=in_channels, 
+                                    out_channels=out_channels,
+                                    kernel_size=conv_kernel_size,
+                                    device=device,
+                                    padding=conv_kernel_size-2)
+        self.norm_layer = nn.BatchNorm2d(num_features=64,
+                                    device=device)
+        self.relu_layer = nn.ReLU()
+        self.max_pool_layer = nn.MaxPool2d((2, 2), ceil_mode=False)
+        
+
+    def forward(self, x):
+        x = self.conv_layer(x)
+        x = self.norm_layer(x)
+        x = self.relu_layer(x)
+        x = self.max_pool_layer(x)
+        return x
+    
+
+class Encoder(nn.Module):
+    def __init__(self, device, x_dim, hid_dim, z_dim):
+        super().__init__()
+        self.modules = [EncoderModule(device, x_dim[0]) if i == 0 
+                                      else EncoderModule(device, in_channels=z_dim)
+                                      for i in range(hid_dim)]
+
+    def forward(self, x):
+        for module in self.modules:
+            x = module(x)
+        return x
+    
+    
---
+++ hw4/src/model.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from encoder import Encoder
+from prepare_data import extract_sample, read_images
+
+
+class ProtoNet(nn.Module):
+    def __init__(self, encoder):
+        """
+        Args:trainx
+            encoder : CNN encoding the images in sample
+            n_way (int): number of classes in a classification task
+            n_support (int): number of labeled examples per class in the support set
+            n_query (int): number of labeled examples per class in the query set
+        """
+        super(ProtoNet, self).__init__()
+        self.encoder = encoder
+
+    def set_forward_loss(self, sample):
+        """
+        Computes loss, accuracy and output for classification task
+        Args:
+            sample (torch.Tensor): shape (n_way, n_support+n_query, (dim)) 
+        Returns:
+            torch.Tensor: shape(2), loss, accuracy and y_hat (predict)
+        """
+        sample_images = sample['images']
+        n_way = sample['n_way']
+        n_support = sample['n_support']
+        n_query = sample['n_query']
+
+        support_samples = sample_images[:, :n_support]
+        query_samples = sample_images[:, n_support:]
+        support_samples = support_samples.reshape(n_way * n_support, *support_samples.size()[2:])
+        query_samples = query_samples.reshape(n_way * n_query, *query_samples.size()[2:])
+        true_classes = np.array([[i] * n_query for i in range(n_way)]).ravel()
+
+        support_samples = self.encoder(support_samples)
+        support_samples = support_samples.reshape(n_way, n_support, *support_samples.size()[-3:])
+        query_samples = self.encoder(query_samples)
+        
+        prototypes = support_samples.mean(1)
+        
+        distances = torch.Tensor([[torch.dist(query_samples[i], prototypes[j]) for j in range(prototypes.size()[0])]
+                                 for i in range(query_samples.size()[0])]) 
+                                  
+        softmax = nn.Softmax(dim=1)
+        probabilities = softmax(-distances)
+
+        y_hat = torch.argmax(probabilities, 1)
+
+        log_softmax = nn.LogSoftmax(dim=1)
+        loss_val = -log_softmax(-distances).mean()
+
+        acc_val = sum(y_hat == true_classes) / y_hat.size()[0]
+        print(sum(y_hat == true_classes))
+
+        return loss_val, {
+            'loss': loss_val.item(),
+            'acc': acc_val.item(),
+            'y_hat': y_hat
+            }
+        
+        
+def load_protonet_conv(device, x_dim=(3, 28, 28), hid_dim=4, z_dim=64):
+    """
+    Loads the prototypical network model
+    Arg:
+      x_dim (tuple): dimension of input image
+      hid_dim (int): dimension of hidden layers in conv blocks
+      z_dim (int): dimension of embedded image
+    Returns:
+      Model (Class ProtoNet)
+    """
+    encoder = Encoder(device, x_dim, hid_dim, z_dim)
+
+    return ProtoNet(encoder)
+
+if __name__ == "__main__":
+    trainx, trainy = read_images('images_background')
+    sample_example = extract_sample(8, 5, 5, trainx, trainy)
+    model = load_protonet_conv(device="cpu")
+    print(model.set_forward_loss(sample_example))
